757075f8cf762f6d89388e81f62182428a382b73
Akinpennu Taiwo <moriyebas@gmail.com>
2025-01-13 20:50:40 +0300

    kj

--- src/Home/LandingPage.jsx
+++ src/Home/LandingPage.jsx
@@ -293,7 +293,7 @@ const LandingPage = () => {
               >
                 {active2?.name}
               </p>
-              <p className="fs-5" style={{ marginTop: "-0.3rem", fontWeight: "500" }}>
+              <p className="fs-5" style={{ marginTop: "-0.3rem", fontWeight: "300" }}>
                 NSU-KSU President
               </p>
             </div>
