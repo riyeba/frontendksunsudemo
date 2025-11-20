bac183c844b659ad4185b8d29c166ded0af75168
Akinpennu Taiwo <moriyebas@gmail.com>
2025-11-20 16:49:03 +0300

    nnownwo

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,16 +1,267 @@
+// // import axios from "axios";
+// // import React, { useEffect, useState } from "react";
+// // import { LazyLoadImage } from "react-lazy-load-image-component";
+
+// // import { HashLink as Link } from "react-router-hash-link";
+
+// // const UserLoginStatus=localStorage.getItem('UserLoginStatus')
+// // const UserLoginStatu=localStorage.getItem('UserLoginStatu')
+
+// // function NavBar() {
+
+
+// //   const [active, setActive] = useState([]);
+// //   const [isLoading, setIsLoading] = useState(true);
+
+// //   useEffect(() => {
+// //     const fetchData = async () => {
+// //       try {
+// //         const response = await axios.get("http://taiwoakinpennu2.pythonanywhere.com/logo/");
+// //         setActive(response.data);
+// //         setIsLoading(false);
+        
+// //       } catch (error) {
+// //         // Handle errors if needed
+// //         console.error("Error fetching data:", error);
+// //         setIsLoading(false);
+// //       }
+// //     };
+    
+
+// //     fetchData();
+// //   }, []);
+
+  
+  
+// // const active2=active.reverse()
+// //  console.log(active2)
+
+ 
+  
+    
+// //   function LogOut(){
+// //     localStorage.removeItem('UserLoginStatus')
+// //     window.location.href='/login'
+// //   }
+
+// //   function LogOutal(){
+// //     localStorage.removeItem('UserLoginStatu') 
+// //     window.location.href='/loginal'     
+// //   }
+
+
+  
+// //   return (
+// //     <nav className="navbar navbar-expand-lg navbar-dark bg-dark  sticky-top">
+// //     <div className="container">
+// //       <div className=" d-flex gap-1">
+// //       <div className="my-1">
+// //       <img src={active2[0]?.photo} width="30" className="rounded-circle"/>
+// //       </div>
+// //       <Link className="navbar-brand text-white" to="/" > NSU-KSU </Link>
+// //       </div>
+      
+// //       <button className="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" >
+// //         <span className="navbar-toggler-icon"></span>
+// //       </button>
+// //       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+// //         <div className="navbar-nav ms-auto">
+// //        {UserLoginStatus==='true'? <Link className="nav-link active text-white fs-6 " aria-current="page" to="/"  ><a id="index.html">Home</a></Link> : <Link className="nav-link active text-white fs-6 " aria-current="page" to="#"  ><a id="index.html">Home</a></Link>}
+// //        {UserLoginStatus==='true' && <Link className="nav-link active text-white fs-6 " aria-current="page" to="/board"  ><a id="index.html">Dashboard</a></Link> }
+// //         {UserLoginStatus==='true'? "" : <Link className="nav-link active text-white fs-6" aria-current="page" to="#excopage">Excos</Link>}
+// //        {UserLoginStatus==='true'? "" : <li className="nav-item dropdown">
+// //           <a className="nav-link dropdown-toggle text-white"  href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+// //             Events
+// //           </a>
+// //           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+// //             <li><Link className="dropdown-item" to="#recentpage" >Recent</Link></li>
+// //             <li><Link className="dropdown-item" to="#upcomingpage">Upcoming</Link></li>
+           
+// //           </ul>
+// //         </li>}
+// //         {/* <Link className="nav-link active text-white fs-6" aria-current="page" to="/home">Recent-Events</Link>
+// //         <Link className="nav-link active text-white fs-6" aria-current="page" to="/home">Upcoming-Events</Link> */}
+// //           {UserLoginStatus==='true'? "" : <Link className="nav-link active text-white fs-6" aria-current="page" to="/home">Login</Link>}
+
+         
+// //           {UserLoginStatus==='true'? <li className="nav-item dropdown">
+// //           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+// //             Active Student
+// //           </a>
+// //           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+             
+// //             <div>
+            
+// //             </div>
+            
+            
+// //             <li onClick={()=>LogOut()}><Link className="dropdown-item text-dark fs-5" href="#">LogOut</Link></li> 
+// //           </ul>
+// //         </li>: ""}
+
+        
+
+// //        {UserLoginStatu=='true'? 
+// //        <div>
+// //        <li className="nav-item dropdown">
+// //           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+// //             Alumni
+// //           </a>
+// //           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+          
+            
+            
+            
+// //             <li onClick={()=>LogOutal()}><Link className="dropdown-item" href="#">LogOut</Link></li>
+// //           </ul>
+// //         </li>
+// //         </div> : ""}
+// //         </div>
+// //       </div>
+// //     </div>
+// //   </nav>
+// //   );
+// // }
+
+// // export default NavBar;
+
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import { LazyLoadImage } from "react-lazy-load-image-component";
+
+// import { HashLink as Link } from "react-router-hash-link";
+
+// const UserLoginStatus=localStorage.getItem('UserLoginStatus')
+// const UserLoginStatu=localStorage.getItem('UserLoginStatu')
+
+// function NavBar() {
+
+
+//   const [active, setActive] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get("http://127.0.0.1:8000/logo/");
+//         setActive(response.data);
+//         setIsLoading(false);
+        
+//       } catch (error) {
+//         // Handle errors if needed
+//         console.error("Error fetching data:", error);
+//         setIsLoading(false);
+//       }
+//     };
+    
+
+//     fetchData();
+//   }, []);
+
+  
+  
+// const active2=active.reverse()
+//  console.log(active2)
+
+ 
+  
+    
+//   function LogOut(){
+//     localStorage.removeItem('UserLoginStatus')
+//     window.location.href='/login'
+//   }
+
+//   function LogOutal(){
+//     localStorage.removeItem('UserLoginStatu') 
+//     window.location.href='/loginal'     
+//   }
+
+
+  
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-dark bg-dark  sticky-top">
+//     <div className="container">
+//       <div className=" d-flex gap-1">
+//       <div className="my-1">
+//       <img src={active2[0]?.photo} width="30" className="rounded-circle"/>
+//       </div>
+//       <Link className="navbar-brand text-white" to="/" > NSU-KSU </Link>
+//       </div>
+      
+//       <button className="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" >
+//         <span className="navbar-toggler-icon"></span>
+//       </button>
+//       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+//         <div className="navbar-nav ms-auto">
+//        {UserLoginStatus==='true'? <Link className="nav-link active text-white fs-6 " aria-current="page" to="/"  ><a id="index.html">Home</a></Link> : <Link className="nav-link active text-white fs-6 " aria-current="page" to="#"  ><a id="index.html">Home</a></Link>}
+//        {UserLoginStatus==='true' && <Link className="nav-link active text-white fs-6 " aria-current="page" to="/board"  ><a id="index.html">Dashboard</a></Link> }
+//         {UserLoginStatus==='true'? "" : <Link className="nav-link active text-white fs-6" aria-current="page" to="#excopage">Excos</Link>}
+//        {UserLoginStatus==='true'? "" : <li className="nav-item dropdown">
+//           <a className="nav-link dropdown-toggle text-white"  href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//             Events
+//           </a>
+//           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+//             <li><Link className="dropdown-item" to="#recentpage" >Recent</Link></li>
+//             <li><Link className="dropdown-item" to="#upcomingpage">Upcoming</Link></li>
+           
+//           </ul>
+//         </li>}
+//         {/* <Link className="nav-link active text-white fs-6" aria-current="page" to="/home">Recent-Events</Link>
+//         <Link className="nav-link active text-white fs-6" aria-current="page" to="/home">Upcoming-Events</Link> */}
+//           {UserLoginStatus==='true'? "" : <Link className="nav-link active text-white fs-6" aria-current="page" to="/home">Login</Link>}
+
+         
+//           {UserLoginStatus==='true'? <li className="nav-item dropdown">
+//           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//             Active Student
+//           </a>
+//           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+             
+//             <div>
+            
+//             </div>
+            
+            
+//             <li onClick={()=>LogOut()}><Link className="dropdown-item text-dark fs-5" href="#">LogOut</Link></li> 
+//           </ul>
+//         </li>: ""}
+
+        
+
+//        {UserLoginStatu=='true'? 
+//        <div>
+//        <li className="nav-item dropdown">
+//           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//             Alumni
+//           </a>
+//           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+          
+            
+            
+            
+//             <li onClick={()=>LogOutal()}><Link className="dropdown-item" href="#">LogOut</Link></li>
+//           </ul>
+//         </li>
+//         </div> : ""}
+//         </div>
+//       </div>
+//     </div>
+//   </nav>
+//   );
+// }
+
+// export default NavBar;
 
 
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import { useNavigate } from "react-router";
 import { HashLink as Link } from "react-router-hash-link";
 
 const UserLoginStatus = localStorage.getItem('UserLoginStatus');
 const UserLoginStatu = localStorage.getItem('UserLoginStatu');
 
 function NavBar() {
-  const navigate = useNavigate();
   const [active, setActive] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isCollapsed, setIsCollapsed] = useState(true);  // State to track navbar collapse
@@ -34,14 +285,12 @@ function NavBar() {
 
   function LogOut() {
     localStorage.removeItem('UserLoginStatus');
-  
-    navigate('/login')
+    window.location.href = '/login';
   }
 
   function LogOutal() {
     localStorage.removeItem('UserLoginStatu');
-    
-    navigate('/loginal')
+    window.location.href = '/loginal';
   }
 
   // Function to handle the collapse toggle when an item is clicked
